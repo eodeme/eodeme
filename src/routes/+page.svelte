6c263a3e5c56e0d198c485f0e470f600bece4a8c
Mu hun <iam@muhun.kim>
2025-05-05 20:42:29 +0900

    체크박스 컴포넌트 데모
    Co-authored-by: DaWonJi <DaWonJi@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Checkbox from "$lib/components/Checkbox.svelte";
+
+	let checked = $state(false);
+
+</script>
+<h1>Welcome</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev</a></p>
+
+<Checkbox {checked} on:change={() => {
+	checked = !checked;
+}} />
+<Checkbox  />
+<Checkbox {checked} disabled />
+<Checkbox  />
+<Checkbox {checked} />
+<Checkbox  />
