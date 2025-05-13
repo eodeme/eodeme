a3b25b1b5e23a4666cf0a33adce5dce8e62d0df9
Mu hun <iam@muhun.kim>
2025-05-13 20:58:14 +0900

    5월 13일 작업
    커스텀 마커 및 마커 오버레이 구현
    
    Co-authored-by: DaWonJi <DaWonJi@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,10 @@
-<svelte:head>
-    <script src="http://dapi.kakao.com/v2/maps/sdk.js?appkey={import.meta.env.VITE_KAKO_API_KEY}"></script> 
-</svelte:head>
-
 <script lang="ts">
 	import '../app.css';
-	
+
 	import { onMount } from 'svelte';
 
 	let mapContainer: HTMLMapElement;
 	let map: kakao.maps.Map;
-	
 
 	onMount(() => {
 		const options = {
@@ -25,11 +20,54 @@
 		map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 		map.setCopyrightPosition(kakao.maps.CopyrightPosition.BOTTOMRIGHT, true);
+
+		const markerSize = 32;
+		const markerImage = new kakao.maps.MarkerImage(
+			'/images/🏁.png',
+			new kakao.maps.Size(markerSize, markerSize),
+			{
+				offset: new kakao.maps.Point(16, 32)
+			}
+		);
+
+		const marker = new kakao.maps.Marker({
+			image: markerImage,
+			position: map.getCenter()
+		});
+		marker.setMap(map);
+
+		const overlay = new kakao.maps.CustomOverlay({
+			map,
+			content: `<article class="w-[128px] bg-white text-black">
+					<img src="/images/hintPicture.png" alt="overlay image" class="" />
+					<div>
+						<div class="">새별오름</div>
+						<button class="">자세히보기</button>
+					</div>
+				</article>`,
+			position: marker.getPosition(),
+			yAnchor: 1.3
+		});
+
+		kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+			const latlng = mouseEvent.latLng;
+
+			if (latlng) {
+				marker.setPosition(latlng);
+				overlay.setPosition(latlng);
+			}
+		});
 	});
 
 	let { children } = $props();
 </script>
 
-<map bind:this={mapContainer} class="block absolute top-0 w-[100vw] h-[100vh]"></map>
+<svelte:head>
+	<script
+		src="http://dapi.kakao.com/v2/maps/sdk.js?appkey={import.meta.env.VITE_KAKO_API_KEY}"
+	></script>
+</svelte:head>
+
+<map bind:this={mapContainer} class="absolute top-0 block h-[100vh] w-[100vw]"></map>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-	import { timer } from "../states/timer.svelte";
+	import { timer } from '../states/timer.svelte';
 
 	let leftTime = $derived(timer.maxTime - timer.currentTime);
 
 	$effect(() => {
 		const interval = setInterval(() => {
 			if (timer.currentTime >= timer.maxTime) {
-				alert("시간이 다 되었습니다.");
+				// alert("시간이 다 되었습니다.");
 				clearInterval(interval);
 				return;
 			}
@@ -19,46 +19,66 @@
 		};
 	});
 
+	let allHint = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+	let currentHintLastIndex = $state(4);
+
+	let currentHit = $derived(allHint.slice(0, currentHintLastIndex + 1));
 </script>
 
-<section class="absolute left-0 bottom-0 w-full z-1">
+<!-- 선언적 표현 -->
+{#each currentHit as hint}
+	<h2 class="z-2">
+		{hint}
+	</h2>
+{/each}
+
+<section class="absolute bottom-0 left-0 z-1 w-full">
 	<article class="flex w-full justify-between">
 		<div class="flex">
 			<img class="w-[466px] rounded-tr-[8px]" src="/images/hintPicture.png" alt="hintImage" />
-			<div class="self-end flex flex-col gap-[8px] text-[16px] text-white max-w-[406px] font-[500]">
-				<p class="bg-black p-[16px]">키워드: 일몰, 일출, 산, 해변, 걷기/등산, 가을, 자연경관, 수국</p>
-				<p class="bg-black p-[16px]">바닷가에 불끈 솟은 산이다. 99개의 작은 봉우리가 모여있다. 
-					2개의 분화구가 존재하는 화산지형이다.</p>
+			<div class="flex max-w-[406px] flex-col gap-[8px] self-end text-[16px] font-[500] text-white">
+				<p class="bg-black p-[16px]">
+					키워드: 일몰, 일출, 산, 해변, 걷기/등산, 가을, 자연경관, 수국
+				</p>
+				<p class="bg-black p-[16px]">
+					바닷가에 불끈 솟은 산이다. 99개의 작은 봉우리가 모여있다. 2개의 분화구가 존재하는
+					화산지형이다.
+				</p>
 			</div>
 		</div>
-		<label for="progress" class="self-end text-[24px] font-[500] mb-[8px] mr-[16px] text-shadow-[0_8px_12px_rgba(0,0,0,0.15),0_4px_4px_rgba(0,0,0,0.30)]">
-			{leftTime > 0 ? '⏳' : '⌛'} {leftTime / timer.interval}초 남음
+		<label
+			for="progress"
+			class="mr-[16px] mb-[8px] self-end text-[24px] font-[500] text-shadow-[0_8px_12px_rgba(0,0,0,0.15),0_4px_4px_rgba(0,0,0,0.30)]"
+		>
+			{leftTime > 0 ? '⏳' : '⌛'}
+			{leftTime / timer.interval}초 남음
 		</label>
 	</article>
-	<progress id="progress" class="block w-full" value={timer.currentTime} max={timer.maxTime}></progress>
+	<progress id="progress" class="block w-full" value={timer.currentTime} max={timer.maxTime}
+	></progress>
 </section>
+
 <style>
-#progress {
-	background-color: rgba(0, 122, 255, 0.30);
-}
+	#progress {
+		background-color: rgba(0, 122, 255, 0.3);
+	}
 
+	#progress::-webkit-progress-bar {
+		background-color: rgba(0, 122, 255, 0.3);
+		border-radius: 0;
+	}
 
-#progress::-webkit-progress-bar {
-  background-color: rgba(0, 122, 255, 0.30);
-  border-radius: 0;
-}
+	#progress::-webkit-progress-value {
+		background-color: #007aff;
+		border-radius: 0;
+	}
 
-#progress::-webkit-progress-value {
-  background-color: #007AFF;
-  border-radius: 0;
-}
+	#progress::-moz-progress-bar {
+		background-color: #007aff;
+	}
 
-#progress::-moz-progress-bar {
-  background-color: #007AFF;
-}
-
-#progress::-ms-fill {
-  background-color: #007AFF;
-  border: none;
-}
+	#progress::-ms-fill {
+		background-color: #007aff;
+		border: none;
+	}
 </style>
