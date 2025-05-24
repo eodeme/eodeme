5602172fb7858aa922c7963cda4c773542ff5ef6
Mu hun <iam@muhun.kim>
2025-05-24 18:39:06 +0900

    제주시 및 서귀포시 나누기

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import { initializeMap } from '$lib/maps';
-	import mapData from '../assets/LSMD_ADM_SECT_UMD_50_202505.json';
 
 	import { onMount } from 'svelte';
-	import { drawPolygon } from '$lib/maps/polygon';
 
 	let mapContainer: HTMLMapElement;
 
 	onMount(() => {
 		const map = initializeMap(mapContainer);
-
-		const areaNameOverlay = new kakao.maps.CustomOverlay({});
-
-		const areas = mapData.features.map((features) => {
-			const coordinates = features.geometry.coordinates[0];
-
-			return {
-				name: features.properties.EMD_NM,
-				path: Array.isArray(coordinates[0][0])
-					? (coordinates as number[][][]).map((ring: number[][]) =>
-							ring.map((coord: number[]) => new kakao.maps.LatLng(coord[1], coord[0]))
-						)
-					: (coordinates as number[][]).map(
-							(coord: number[]) => new kakao.maps.LatLng(coord[1], coord[0])
-						)
-			};
-		});
-		for (const area of areas) {
-			drawPolygon({ map, area, overlay: areaNameOverlay });
-		}
 	});
 
 	let { children } = $props();
